8efe0735853c64229606fb232a5a1a1b8e6fe04f
Ida Hallner <bee_ida@hotmail.com>
2019-01-22 11:38:27 +0100

    install babel, terser, sourcemap

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,14 @@
+
 const browsersync = require('rollup-plugin-browsersync');
 const postcss = require('rollup-plugin-postcss');
 const postcssNormalize = require('postcss-normalize');
 const autoPrefixer = require('autoprefixer');
+const cssNano = require('cssnano');
+const babel = require('rollup-plugin-babel');
+const resolve = require('rollup-plugin-node-resolve');
+const commonJs = require('rollup-plugin-commonjs');
+const {terser} = require("rollup-plugin-terser");
+
 
 const isProduction = process.env.NODE_ENV === 'production';
 const isDevelopment = isProduction === false;
@@ -10,18 +17,25 @@ module.exports = {
   input: 'src/scripts/index.js',
   output: {
   file: 'public/giphy.js',
-  format: 'iife'
+  format: 'iife',
+  sourcemap: true
   },
 
   plugins:
   [
-    (isDevelopment && browsersync({server: 'public'})),
     postcss ({
       extract: true,
+      sourceMap: isDevelopment,
       plugins:[
         postcssNormalize(),
-        autoPrefixer()
+        autoPrefixer(),
+        cssNano(),
       ],
-      })
+    }),
+    babel(),
+    resolve(),
+    commonJs(),
+    terser(),
+    (isDevelopment && browsersync({server: 'public'})),
     ]
 };
